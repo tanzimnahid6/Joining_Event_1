3f8c4a073874ae81ebe3f9081e8c6419bd4c124d
Md Aljubayer <tanzimnahid6@gmail.com>
2024-07-17 13:47:29 +0600

    add loading components

--- components/loading/Loading.jsx
+++ components/loading/Loading.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Loading = () => {
     return (
-        <div>
-            Loading event...
-        </div>
+      <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75">
+        <div className="animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-white"></div>
+      </div>
     );
-};
-
-export default Loading;
+  };
+  
+  export default Loading;
